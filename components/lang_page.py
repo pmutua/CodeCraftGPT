b46498fd10e8c8dcb3dbc9b5eaa36eb534745ca5
pmutua <pmutua@live.com>
2023-11-14 18:26:01 +0300

    feat: :sparkles: code refactoring and improvement of prompt

--- components/lang_page.py
+++ components/lang_page.py
@@ -1,21 +1,47 @@
+"""
+LangLink - Code Translation and Cross-Language Compatibility
+
+Overcome language barriers with LangLink, an AI-powered tool facilitating smooth code translation
+between programming languages. Developers can confidently migrate codebases, ensuring compatibility
+and seamless transitions across different languages.
+"""
+
 import streamlit as st
 from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import (
-    ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate)
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate)
+
+from data.programming_languages import PROGRAMMING_LANGUAGES
 
 def show_lang_page(openai_api_key):
+    """
+    Displays the LangLink page for code translation.
+
+    Parameters:
+    - openai_api_key (str): The API key for OpenAI.
+
+    Returns:
+    None
+    """
     st.title("LangLink - Code Translation and Cross-Language Compatibility")
-    
-    st.markdown('Overcome language barriers with LangLink, an AI-powered tool facilitating smooth code translation between programming languages. Developers can confidently migrate codebases, ensuring compatibility and seamless transitions across different languages.')
+
+    st.markdown('Overcome language barriers with LangLink, an AI-powered tool facilitating smooth '
+                'code translation between programming languages. Developers can confidently migrate '
+                'codebases, ensuring compatibility and seamless transitions across different languages.')
 
     with st.form(key="lang_form"):
         source_code = st.text_area("Enter source code")
-        target_language = st.text_input("Enter the target language")
+        target_language = st.selectbox("Select programming language", PROGRAMMING_LANGUAGES)
 
         submit_button = st.form_submit_button(label='Submit')
 
         if submit_button:
+            
+            st.text(f"Translating code snippet to {target_language}................✨")
+
             chat = ChatOpenAI(
                 model="gpt-3.5-turbo-16k",
                 openai_api_key=openai_api_key,
@@ -34,4 +60,6 @@ def show_lang_page(openai_api_key):
             chain = LLMChain(llm=chat, prompt=chat_prompt)
             result = chain.run(source_code=source_code,
                                target_language=target_language)
+            
             st.markdown(result)
+            
