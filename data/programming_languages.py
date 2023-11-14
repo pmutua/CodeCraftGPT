0d21ba788ec69a107ff944edcce2618aa4dcb37c
pmutua <pmutua@live.com>
2023-11-14 18:28:41 +0300

    chore: Add data folder containing coding styles, programming langs & testinglibs

---
+++ data/programming_languages.py
@@ -0,0 +1,6 @@
+PROGRAMMING_LANGUAGES = (
+    "Python", "JavaScript", "Java", "C++", "C#", "Ruby", "Swift", "Go", "PHP", "Rust",
+    "Kotlin", "TypeScript", "Scala", "Haskell", "Perl", "Objective-C", "Dart", "R", "Groovy",
+    "Elixir", "Lua", "Julia", "Shell", "HTML", "CSS", "SQL", "MATLAB", "CoffeeScript", "F#",
+    "Clojure", "Assembly", "Lisp", "Cobol", "Fortran", "Racket", "Ada", "Prolog"
+)
