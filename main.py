6285ee88cd53084406a813f723cd7665b79ed4c5
Philip Mutua <pmutua@live.com>
2023-12-29 09:06:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ from components import (
 )
 
 def set_openai_api_key():
-    api_key = st.text_input("Enter your OpenAI API key:")
+    st.sidebar.title("OpenAI API Key")
+    api_key = st.sidebar.text_input("Enter your OpenAI API key:")
     if api_key:
         os.environ["OPENAI_API_KEY"] = api_key
     else:
@@ -25,36 +26,43 @@ def main():
         layout="wide"
     )
 
-    set_openai_api_key()
+    # Check if OpenAI API key is set
+    if "OPENAI_API_KEY" not in os.environ:
+        set_openai_api_key()
 
-    with st.sidebar:
-        selected = option_menu(
-            menu_title="CodeCraftGPT",
-            options=[
-                "Home", "RefactorRite", "StyleSculpt", "TestGenius", 
-                "LangLink", "CodeDocGenius", "Database"
-            ],
-            icons=[
-                'house', 'gear', 'palette', 'clipboard2-pulse', 
-                'code-slash', 'file-text', 'database'
-            ],
-            default_index=0
-        )
+    # Display sidebar items only if API key is set
+    if "OPENAI_API_KEY" in os.environ:
+        with st.sidebar:
+            selected = option_menu(
+                menu_title="CodeCraftGPT",
+                options=[
+                    "Home", "RefactorRite", "StyleSculpt", "TestGenius", 
+                    "LangLink", "CodeDocGenius", "Database"
+                ],
+                icons=[
+                    'house', 'gear', 'palette', 'clipboard2-pulse', 
+                    'code-slash', 'file-text', 'database'
+                ],
+                default_index=0
+            )
 
-    pages = {
-        "RefactorRite": refactor_page.show_refactor_page,
-        "StyleSculpt": style_page.show_style_page,
-        "TestGenius": test_page.show_test_page,
-        "LangLink": lang_page.show_lang_page,
-        "CodeDocGenius": code_documentation_page.show_doc_page,
-        "Database": database_page.show_database_page,
-        "Home": home.show_home_page
-    }
+        # Display pages based on selection
+        pages = {
+            "RefactorRite": refactor_page.show_refactor_page,
+            "StyleSculpt": style_page.show_style_page,
+            "TestGenius": test_page.show_test_page,
+            "LangLink": lang_page.show_lang_page,
+            "CodeDocGenius": code_documentation_page.show_doc_page,
+            "Database": database_page.show_database_page,
+            "Home": home.show_home_page
+        }
 
-    if selected in pages:
-        pages[selected]()
+        if selected in pages:
+            pages[selected]()
+        else:
+            st.error("Page not found!")
     else:
-        st.error("Page not found!")
+        st.warning("Please enter your OpenAI API key to access pages.")
 
 if __name__ == "__main__":
     main()
